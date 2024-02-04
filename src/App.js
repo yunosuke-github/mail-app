cd3d2358db8fe67818dec084e54e0996f294da77
naito <yunosuke.github@gmail.com>
2024-02-04 21:29:57 +0900

    layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {useState} from 'react';
+import {
+  LaptopOutlined,
+  MailOutlined,
+  NotificationOutlined,
+  SendOutlined,
+  StarOutlined,
+  UserOutlined
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+const { Header, Content, Sider } = Layout;
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
 }
 
+const menuItems = [
+  getItem('受信トレイ', 'receiving-tray', <MailOutlined />, null, ''),
+  getItem('スター付き', 'starred', <StarOutlined />, null, ''),
+  getItem('送信済み', 'sent', <SendOutlined />, null, ''),
+  getItem('ラベル', 'labels', null, [], 'group'),
+]
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Header
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div style={{
+          color: '#FFFFFF',
+          fontSize: 24
+        }}>Mail App</div>
+      </Header>
+      <Layout>
+        <Sider
+          width={200}
+          style={{
+            background: colorBgContainer,
+          }}
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+        >
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['receiving-tray']}
+            defaultOpenKeys={['receiving-tray']}
+            style={{
+              height: '100%',
+              borderRight: 0,
+            }}
+            items={menuItems}
+          />
+        </Sider>
+        <Layout
+          style={{
+            padding: '24px 24px 24px',
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Layout>
+      </Layout>
+    </Layout>
+  );
+};
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Content} from "antd/es/layout/layout";
+
+export default function Home() {
+  return (
+    <Content
+      style={{
+        padding: 24,
+        margin: 0,
+        minHeight: 280,
+        background: 'white',
+        borderRadius: 5,
+      }}
+    >
+      <div>hoge</div>
+    </Content>
+  )
+}
